a686363a09d694671818e050f4fab26c9ca8bc0f
Tyler Chia <129912803+tylerrchia@users.noreply.github.com>
2026-01-28 12:31:07 -0800

    Create csv_to_rds.R

---
+++ scripts/csv_to_rds.R
@@ -0,0 +1,15 @@
+library(readr)
+
+if (file.exists("data/forcedecks.csv")) {
+  saveRDS(
+    read_csv("data/forcedecks.csv", show_col_types = FALSE),
+    "data/forcedecks.rds"
+  )
+}
+
+if (file.exists("data/nordbord.csv")) {
+  saveRDS(
+    read_csv("data/nordbord.csv", show_col_types = FALSE),
+    "data/nordbord.rds"
+  )
+}
