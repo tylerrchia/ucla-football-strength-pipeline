3bae9d40f7a8596bc29207635ec0f19d45f3fc38
Tyler Chia <129912803+tylerrchia@users.noreply.github.com>
2026-02-02 15:43:37 -0800

    added append logic

--- scripts/catapult_import.R
+++ scripts/catapult_import.R
@@ -17,26 +17,83 @@ library(readr)
 token <- ofCloudCreateToken(sToken = Sys.getenv("CATAPULT_API_TOKEN"),
                             sRegion = "America")
 
+# -------------------------------------------------------------------------------
 # list of teams available
 teams <- ofCloudGetTeams(token)
-glimpse(teams)
 
 # list of parameters available
 parameters <- ofCloudGetParameters(token)
-glimpse(parameters)
 
 # list of activities available
-activities <- ofCloudGetActivities(token)
-glimpse(activities)
+activities <- ofCloudGetActivities(token) # pull all activities
+# filter only activities after the first 2026 training session
+activities <- activities %>%
+  mutate(modified_at = ymd_hms(modified_at, tz = "UTC")) %>% 
+  select(id, name, modified_at) %>% 
+  filter(modified_at > ymd_hms("2026-01-26 21:04:29", tz = "UTC"))
+# -------------------------------------------------------------------------------
+# initial pull of all stats from 1/26/2026
+# catapult_initial <- map_dfr(activities$id[1:5], function(act_id) {
+#   ofCloudGetStatistics(
+#     token, 
+#     params = c(
+#       "athlete_name", "date", 
+#       "total_distance", "total_duration", "total_player_load", "max_vel", 
+#       "hsr_efforts", "period_id", "period_name", "activity_name"
+#     ), 
+#     groupby = c("athlete", "period", "activity"), 
+#     filters = list(
+#       name = "activity_id",
+#       comparison = "=",
+#       values = act_id
+#     )
+#   )
+# })
 
-stats_df <- ofCloudGetStatistics(
-  token, 
-  params = c("athlete_name", "date", "start_time", "end_time", "position_name", 
-             "total_distance", "total_duration", "total_player_load", "max_vel", 
-             "hsr_efforts", "max_heart_rate", "mean_heart_rate", 
-             "period_id", "period_name", "activity_name"), 
-  groupby = c("athlete", "period", "activity"), 
-  filters = list(name = "activity_id",
-                 comparison = "=",
-                 values = activities$id[1])
-)
+# pull last 3 activities
+activities_recent <- activities %>%
+  mutate(modified_at = ymd_hms(modified_at, tz = "UTC")) %>%
+  arrange(desc(modified_at)) %>%
+  slice(1:3)
+
+catapult_append <- map_dfr(activities_recent$id, function(act_id) {
+  ofCloudGetStatistics(
+    token, 
+    params = c(
+      "athlete_name", "date", 
+      "total_distance", "total_duration", "total_player_load", "max_vel", 
+      "hsr_efforts", "period_id", "period_name", "activity_name"
+    ), 
+    groupby = c("athlete", "period", "activity"), 
+    filters = list(
+      name = "activity_id",
+      comparison = "=",
+      values = act_id
+    )
+  )
+})
+
+
+# -------------------------------------------------------------------------------
+# APPENDING TO DATA FOLDER
+catapult_path <- "data/catapult.csv"
+
+if (file.exists(catapult_path)) {
+  catapult_existing <- read_csv(catapult_path, show_col_types = FALSE)
+  
+  catapult_final <- bind_rows(
+    catapult_existing,
+    catapult_append
+  ) %>%
+    distinct(
+      athlete_name,
+      date,
+      activity_name,
+      .keep_all = TRUE
+    )
+} else {
+  catapult_final <- catapult_initial
+}
+
+write_csv(catapult_final, catapult_path)
+
